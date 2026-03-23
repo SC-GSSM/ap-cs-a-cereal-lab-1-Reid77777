545b2c85fcab64655c1431b1689b88de2b3d512f
Reid77777 <rkaikini26@governors.school>
2026-03-23 02:10:49 +0000

    Done

--- src/main/java/CerealRunner.java
+++ src/main/java/CerealRunner.java
@@ -7,10 +7,13 @@ import java.io.FileReader;
 public class CerealRunner
 {
     // create a private instance variable to store an ArrayList of Cereal objects
+    private ArrayList<Cereal> cereals;
+
 
     public CerealRunner(String fileName)
     {
         // instantiate the ArrayList
+        cereals = new ArrayList<>();
 
         try
         {
@@ -22,12 +25,21 @@ public class CerealRunner
 
                 // use the split method to parse the data into an array of
                 //   String objects
+                String[] data = myStr.split(",");
 
                 // go through each String array element and save it
                 //   into the appropriate variable which will be used to
                 //   create a Cereal object
+                String name = data[0];
+                int calories = Integer.parseInt(data[1]);
+                double fiber = Double.parseDouble(data[2]);
+                double carbohydrates = Double.parseDouble(data[3]);
+                double cups = Double.parseDouble(data[4]);
+
 
                 // create a new Cereal object, and add it to the ArrayList
+                Cereal cereal = new Cereal(name, calories, fiber, carbohydrates, cups);
+                cereals.add(cereal);
 
             }
             scan.close();
